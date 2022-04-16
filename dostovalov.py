d5acfe556c7d96fec4bc27fdefaffd352b78bf04
Student <jen_dostovalov@mail.ru>
2022-04-16 10:28:04 +0500

    exam

--- dostovalov.py
+++ dostovalov.py
@@ -5,9 +5,10 @@ def exam(first, second):
     # return
 
 
-exam(2, 2)
+exam(5, 2)
 
 exam_2 = lambda first_2, second_2: first_2 + second_2
 # exam_3 = lambda first_2, second_2: first_2 * second_2
 
 print(exam_2(5, 6))
+# print(exam_3(5, 6))
